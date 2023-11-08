7f781086ef945132126f887d1d0c938392d6e098
AmeeYuki <yukinooame@gmail.com>
2023-11-08 22:13:40 +0700

    List pet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@fontsource/roboto/400.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Route, Routes } from "react-router-dom";
+import Blog from "./components/Blog";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import PetList from "./components/PetList";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <div className="mt-3 mb-5">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Blog" element={<Blog />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+          <Route path="/Pet List" element={<PetList />}></Route>
+          {/* <Route path="/detail/:id" element={<Detail />}></Route>
+        <Route path="/addNewStaff" element={<AddStaff />}></Route>
+        <Route path="/updateStaff/:id" element={<UpdateStaff />}></Route> */}
+        </Routes>
+      </div>
+
+      <Footer />
     </div>
   );
 }
